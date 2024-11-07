d2d30d42af5cf8c64980236f7c10a56d49892985
Tangerie <josh.cavill01@gmail.com>
2024-11-07 14:23:05 +1100

    added 1p

--- src/test.ts
+++ src/test.ts
@@ -1,14 +1,28 @@
 import "jsr:@std/dotenv/load";
 import { agentboxAuthenticate } from "./agentbox.ts";
 import { corelogicLogin } from "./corelogic.ts";
+import { configure, getSecrets } from "./1password.ts";
 
-// console.log(await agentboxAuthenticate(
-//     Deno.env.get("AGENTBOX_USERNAME")!, 
-//     Deno.env.get("AGENTBOX_PASSWORD")!,
-//     { AgentboxBaseUrl: Deno.env.get("AGENTBOX_BASE")! }
-// ))
+await configure(
+    Deno.env.get("OP_TOKEN")!,
+    Deno.env.get("OP_NAME")!,
+    Deno.env.get("OP_VERSION")!,
+)
+
+console.log(await getSecrets("Automation", "CoreLogic", "username", "password"));
+
+const [abUser, abPass] = await getSecrets("Automation", "Reapit", "username", "password");
+
+console.log(await agentboxAuthenticate(
+    abUser, 
+    abPass,
+    { AgentboxBaseUrl: Deno.env.get("AGENTBOX_BASE")! }
+))
+
+let [clUser, clPass] = await getSecrets("Automation", "CoreLogic", "username", "password");
 
 console.log(await corelogicLogin(
-    Deno.env.get("CORELOGIC_USERNAME")!,
-    Deno.env.get("CORELOGIC_PASSWORD")!
+    clUser,
+    clPass
 ))
+
